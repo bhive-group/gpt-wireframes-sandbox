3547b1a1fd231452605aba985f394562af7c5b16
gaurav-bhive <gaurav@bhiveworkspace.com>
2025-02-18 07:08:50 +0000

    update index content

--- github_post_build.sh
+++ github_post_build.sh
@@ -20,25 +20,20 @@ cat > $OUTPUT_FILE <<EOL
   <body>
     <h1>GPT Wireframes</h1>
     <div class="container">
-    <div class="card" style="width: 80vw;">
-  <div class="card-body">
-    <h5 class="card-title">List of HTML Wireframes</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Links will open in new tab</h6>
-    <ul class="list-group list-group-flush">
+      <h4 class="card-title">List of HTML Wireframes</h4>
+      <h5 class="card-subtitle mb-2 text-muted">Links will open in new tab</h5>
 EOL
 
 # Find all HTML files except index.html and append to the index.html file
 find . -type f -name "*.html" ! -name "index.html" | sort | while read file; do
     filename=$(basename "$file")
-    echo "    <li class='list-group-item'><a href='./$filename'>$filename</a></li>" >> $OUTPUT_FILE
+    echo "<div class='card' style='width: 100vw;'> <iframe src='./$filename'></iframe>" >> $OUTPUT_FILE
+    echo "<div class='card-body'><p><a class='card-link' href='./$filename' target='_blank'>$filename</a></p></div>" >> $OUTPUT_FILE
+    echo "</div>" >> $OUTPUT_FILE
 done
 
 # Close the HTML file
 cat >> $OUTPUT_FILE <<EOL
-    </ul>
-    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-    <!-- <a href="#" class="card-link">Card link</a> -->
-    <!-- <a href="#" class="card-link">Another link</a> -->
     </div>
   </div>
 </div>
